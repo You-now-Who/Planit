cd91dc12299ec3a65e3ba7de41e3f5c0ea9a5304
You-now-Who <yashyadav.sr@gmail.com>
2023-03-26 17:07:15 +0530

    Added more stuff for appwrite env, and pushed the final look of the dashboard

--- planit/src/pages/index.js
+++ planit/src/pages/index.js
@@ -1,19 +1,55 @@
 import Navbar from "@/components/Navbar"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <>
       <Navbar/>
 
-      <main>
-        <h1>Planit</h1>
-        <h2>Your Event Management Solution</h2>
-        <p>Planit helps you plan and manage your events with ease. From corporate events to weddings and parties, we've got you covered.</p>
-        <a href="#">Get Started</a>
-      </main>
-      <footer>
-        <p>&copy; 2023 Planit. All rights reserved.</p>
-      </footer>
+      <main className="py-10 px-10">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+            Welcome to Planit!
+          </h2>
+          <p className="text-gray-600 mb-8">
+            We help you manage and organize your events seamlessly.
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="bg-white rounded-lg shadow-md p-6">
+              <h3 className="text-xl font-semibold text-gray-800 mb-4">
+                Create an Event
+              </h3>
+              <p className="text-gray-600 mb-4">
+                Easily create an event and add details such as date, time,
+                location, and more.
+              </p>
+              {/* <Link
+                to="/create-event"
+                className="block text-center py-2 px-4 rounded-md bg-indigo-600 text-white font-medium hover:bg-indigo-700"
+              >
+                Create Event
+              </Link> */}
+            </div>
+            <div className="bg-white rounded-lg shadow-md p-6">
+              <h3 className="text-xl font-semibold text-gray-800 mb-4">
+                View Upcoming Events
+              </h3>
+              <p className="text-gray-600 mb-4">
+                Stay on top of your upcoming events with our easy-to-use
+                calendar view.
+              </p>
+              {/* <Link
+                to="/calendar"
+                className="block text-center py-2 px-4 rounded-md bg-indigo-600 text-white font-medium hover:bg-indigo-700"
+              >
+                View Calendar
+              </Link> */}
+            </div>
+          </div>
+        </main>
+        <footer className="py-6 text-center text-gray-500 text-sm">
+          &copy; 2023 Planit. All rights reserved.
+        </footer>
+      
 
     </>
   )
